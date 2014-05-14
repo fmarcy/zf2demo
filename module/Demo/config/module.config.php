64e70c2fd56063cbd5a48210f762f4ee4c3198ce
Freddy MARCY <freddymarcy@mbp-de-freddy.lan>
2014-05-14 19:44:05 +0200

    Create demo route

--- module/Demo/config/module.config.php
+++ module/Demo/config/module.config.php
@@ -1,5 +1,30 @@
 <?php
 
 return array(
-	
+	'router' => array(
+		'routes' => array(
+			'demo' => array(
+				'type' => 'Literal',
+				'options' => array(
+					'route' => '/demo',
+					'defaults' => array(
+						'controller' => 'demo-index-controller',
+						'action' => 'index',
+					),
+				),
+			),
+		),
+	),
+		
+	'controllers' => array(
+		'invokables' => array(
+			'demo-index-controller' => 'Demo\Controller\IndexController',
+		),				
+	),
+		
+	'view_manager' => array(
+		'template_path_stack' => array(
+			__DIR__ . '/../view',
+		),	
+	),
 );
---
+++ module/Demo/src/Demo/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Demo\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+class IndexController extends AbstractActionController
+{
+	public function indexAction()
+	{
+		
+	}
+}
